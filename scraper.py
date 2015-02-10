1e16b548a7608413821b28f950c2a142ab586c5f
Duncan Parkes <duncan.parkes@gmail.com>
2015-02-10 22:15:07 +0000

    Update scraper.py
    Fetch page and print stuff to see if I understand morph.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 # This is a template for a Python scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
@@ -21,3 +21,8 @@
 # on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
 # is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 # has at least a table called data.
+
+seating_plan_html = scraperwiki.scrape('http://www.assembly.wales/en/memhome/Pages/mem-seating-plan.aspx')
+sp_root = lxml.html.fromstring(html)
+for a in sp_root.cssselect('table a'):
+  print a.href
